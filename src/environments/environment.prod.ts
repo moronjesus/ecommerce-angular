e47255d922d7fc44548ddf0da9a08a7481c5f948
moronjesus <68825379+moronjesus@users.noreply.github.com>
2021-12-27 13:35:28 -0400

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,24 @@
+// export const environment = {
+//   production: false,
+//   firebase: {
+//     apiKey: "AIzaSyC5kr77VEU-FnklR5DBn3bPwmtmpEjYde4",
+//     authDomain: "embryo-version-2.firebaseapp.com",
+//     databaseURL: "https://embryo-version-2.firebaseio.com",
+//     projectId: "embryo-version-2",
+//     storageBucket: "embryo-version-2.appspot.com",
+//     messagingSenderId: "73552048992"
+//   }
+// };
+
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyASRILuKj28yCkn4AnP9mbKwsYdeqmKsWs",
+    authDomain: "embryo-testing-e46bc.firebaseapp.com",
+    databaseURL: "https://embryo-testing-e46bc.firebaseio.com",
+    projectId: "embryo-testing-e46bc",
+    storageBucket: "",
+    messagingSenderId: "532337713049"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,36 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+// export const environment = {
+//   production: false,
+//   firebase: {
+//     apiKey: "AIzaSyC5kr77VEU-FnklR5DBn3bPwmtmpEjYde4",
+//     authDomain: "embryo-version-2.firebaseapp.com",
+//     databaseURL: "https://embryo-version-2.firebaseio.com",
+//     projectId: "embryo-version-2",
+//     storageBucket: "embryo-version-2.appspot.com",
+//     messagingSenderId: "73552048992"
+//   }
+// };
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyASRILuKj28yCkn4AnP9mbKwsYdeqmKsWs",
+    authDomain: "embryo-testing-e46bc.firebaseapp.com",
+    databaseURL: "https://embryo-testing-e46bc.firebaseio.com",
+    projectId: "embryo-testing-e46bc",
+    storageBucket: "",
+    messagingSenderId: "532337713049"
+  }
+};
